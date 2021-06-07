47a577d0f5d6be989c1a347d4392d98b7150d5bb
Gavin <gavinwzmails@gmail.com>
2021-06-07 11:27:01 +0800

    Queue

--- src/Test.cpp
+++ src/Test.cpp
@@ -1,27 +1,14 @@
 #include "../include/ds.h"
 #include<stdlib.h>
-using namespace my::shared_stack;
+using namespace my::link_queue;
 
 
 int main(){
-    SharedStack stack;
-    init(stack);
-    push(stack,2,2);
-    push(stack,3,1);
-    push(stack,4,1);
-    // clear(stack);
-    
-    push(stack, 4,1);
-    // cout << get_top(stack);
-    // cout << get_top(stack);
-    cout << pop(stack,2);
-    cout << pop(stack,2);
-    cout << pop(stack,2);
-    cout << pop(stack,2);
-    cout << pop(stack,2);
-    // cout << pop(stack);
-    // cout << pop(stack);
-    // cout << pop(stack);
-
+    LinkQueue queue;
+    init(queue);
+    enter(queue, 2);
+    enter(queue, 3);
+    clear(queue);
+    cout << my::link_queue::is_empty(queue);
 
 }
