40fa45c512f23663df8e0f541caa8166283d553c
Gavin <gavinwzmails@gmail.com>
2021-06-24 21:21:30 +0800

    二叉排序树

--- include/bi_tree.h
+++ include/bi_tree.h
@@ -16,7 +16,7 @@ public:
         this->right = nullptr;
     }
 
-    // void pre_init(BiTreeNode** root){
+    // void pre_init(BiTreeNode** root){  //以先根方式建立二叉树
     //     T value;
     //     cin >> value;
     //     if(value == '#')
@@ -31,7 +31,7 @@ public:
     //     }
     // }
 
-    BiTreeNode* pre_init(){
+    BiTreeNode* pre_init(){  //以先根方式建立二叉树
         T value;
         cin >> value;
         BiTreeNode* node = new BiTreeNode;
@@ -44,7 +44,7 @@ public:
         return node;
     }
 
-    void pre_order(BiTreeNode* root){
+    void pre_order(BiTreeNode* root){  //先序遍历
         if(root != nullptr){
             cout << root->value << " ";
             pre_order(root->left);
@@ -52,7 +52,7 @@ public:
         }
     }
 
-    void in_order(BiTreeNode* root){
+    void in_order(BiTreeNode* root){  //中序遍历
         if(root != nullptr){
             in_order(root->left);
             cout << root->value << " ";
@@ -60,7 +60,7 @@ public:
         }
     }
 
-    void post_order(BiTreeNode* root){
+    void post_order(BiTreeNode* root){  //后序遍历
         if(root != nullptr){
             post_order(root->left);
             post_order(root->right);
@@ -68,7 +68,7 @@ public:
         }
     }
 
-    void level_order(BiTreeNode* root){
+    void level_order(BiTreeNode* root){  //二叉树的层次遍历
         LinkQueue<BiTreeNode*> queue;
         init(queue);
         enter(queue, root);
@@ -81,4 +81,27 @@ public:
                 enter(queue, root->right);
         }
     }
+
+    void sort_init(BiTreeNode<int>** root){  //二叉排序树的创建
+        T value;
+        while(cin >> value && value != 9999){
+            sort_insert(&(*root), value);
+        }
+
+    }
+
+    void sort_insert(BiTreeNode<int>** root, T value){  //二叉排序树的插入操作
+        if((*root) == nullptr){
+            (*root) = new BiTreeNode<int>();
+            (*root)->value = value;
+        }
+        else{
+            if((*root)-> value > value){
+                sort_insert(&(*root)->left, value);
+            }
+            else{
+                sort_insert(&(*root)->right, value);
+            }
+        }
+    }   
 };
